49846a30391b3b0c7ea549112371173977275809
NuclearIcecream <NuclearIcecream@users.noreply.github.com>
2020-02-09 23:40:07 -0500

    nothing changed

--- webGL_ProjScript.js
+++ webGL_ProjScript.js
@@ -319,11 +319,11 @@ var runEngine = function(vertexShaderCode, fragmentShaderCode, inputModelJSON, t
     var ayaTexture = gl.createTexture();
     var img = document.getElementById("AyaImage");
     gl.bindTexture(gl.TEXTURE_2D, ayaTexture);
-    //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
     gl.generateMipmap(gl.TEXTURE_2D)
 
