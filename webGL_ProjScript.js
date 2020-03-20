b3cdc546874a28534dc6d9db2a4c414582bf3610
NuclearIcecream <NuclearIcecream@users.noreply.github.com>
2020-03-20 19:10:37 -0400

    fixed caps

--- webGL_ProjScript.js
+++ webGL_ProjScript.js
@@ -107,7 +107,7 @@ var initEngine = function () {
     async function getAyaImage () {
         return new Promise ( (resolve, reject) => {
             var image = new Image();
-            image.src = '![](091_W_Aya_2k_01.jpg)'
+            image.src = '![](091_W_Aya_2K_01.jpg)'
             console.log ('image try');
             image.onload = () => resolve (image)
             image.onerror = () => reject (new Error ("Could not reslove image"))
